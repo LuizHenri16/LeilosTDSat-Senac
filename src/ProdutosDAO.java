7e13e6686e79844e8a398d6668869107cbed6279
Luiz Henrique <lbastos722@gmail.com>
2025-04-29 15:23:05 -0300

    add: cadastro Produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,44 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        String sql = "INSERT INTO produtos(nome, valor, status) value(?,?,?)";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement(sql);
+
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            prep.execute();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso");
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar o produto no banco de dados" + e.getMessage());
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,10 +1,7 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
-
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -15,18 +12,20 @@ import javax.swing.JOptionPane;
  *
  * @author Adm
  */
+
 public class conectaDAO {
     
     public Connection connectDB(){
         Connection conn = null;
         
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?useSSL=true&user=root&password=Luizinho1604@");
             
-        } catch (SQLException erro){
+        } catch ( ClassNotFoundException | SQLException erro){
+            System.out.println(erro);
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
+        } 
         return conn;
     }
     
